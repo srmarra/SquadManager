98e6b9e0cd9563164237b1471eab9cf7e26df7dc
srmarra <social@herickmarra.com.br>
2022-09-03 11:18:38 -0300

    adicionando o Swagger
    Nessa Atualização eu adiicionei e configurei o Swagger

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="username"> Username do usuario</param>
+        /// <param name="password">senha do usuario</param>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Login(string username, string password)
+        {
+            return Ok(new { response = "Está Logado"});
+        }
+    }
+}
